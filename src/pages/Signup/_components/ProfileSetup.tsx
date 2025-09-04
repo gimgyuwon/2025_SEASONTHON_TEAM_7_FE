ee51cacaf8b79b8bbd524fd73d716b9dd3ce615f
hardwoong <dew04300@naver.com>
2025-09-04 11:08:23 +0900

    refact: 회원가입 컴포넌트 분리

---
+++ src/pages/Signup/_components/ProfileSetup.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import AgeRangeSelector from './AgeRangeSelector';
+
+interface ProfileSetupProps {
+  formData: {
+    nickname: string;
+    ageRange: string;
+  };
+  onInputChange: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
+  onAgeChange: (age: string) => void;
+  onNext: () => void;
+}
+
+const ProfileSetup: React.FC<ProfileSetupProps> = ({
+  formData,
+  onInputChange,
+  onAgeChange,
+  onNext
+}) => {
+  return (
+    <>
+      <div className="subT">프로필 설정</div>
+      
+      <form className="extra-info-form">
+        <div className="form-group">
+          <label htmlFor="nickname">닉네임</label>
+          <input
+            type="text"
+            id="nickname"
+            name="nickname"
+            value={formData.nickname}
+            onChange={onInputChange}
+            placeholder="2-8자 한글"
+            required
+            maxLength={16}
+          />
+        </div>
+
+        <AgeRangeSelector
+          selectedAge={formData.ageRange}
+          onAgeChange={onAgeChange}
+        />
+        
+        <div className="btn-primary">
+          <button 
+            type="button" 
+            onClick={onNext}
+          >
+            다음
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default ProfileSetup;
