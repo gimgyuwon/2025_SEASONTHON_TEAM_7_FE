1654e43d681980befa63367c127058a27b2f4e4b
hardwoong <dew04300@naver.com>
2025-09-04 11:08:49 +0900

    chore: 스타일 정리 및 라우팅 설정 업데이트

--- src/pages/Login/_components/kakao.tsx
+++ src/pages/Login/_components/kakao.tsx
@@ -3,7 +3,8 @@ import KakaoIcon from '@/assets/kakaoIcon.svg';
 const Kakao = () => {
   const handleLogin = () => {
     // Spring Security OAuth2 엔드포인트로 리다이렉트, 추후 배포 시 다른 주소로 변경
-    window.location.href = 'http://localhost:8080/api/login/oauth2/authorization/kakao';
+    window.location.href = 'http://34.47.125.208:8080/api/oauth2/authorization/kakao';
+    // window.location.href = 'http:/localhost:8080/api/oauth2/authorization/kakao';
   };
 
   return (
