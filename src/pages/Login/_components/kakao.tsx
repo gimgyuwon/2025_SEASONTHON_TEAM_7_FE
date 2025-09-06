3407e6f611b46095d604ed93487f5c5d7e968ce3
hardwoong <dew04300@naver.com>
2025-09-06 18:32:59 +0900

    feat: 로그인 페이지 UI 개선 및 로고 업데이트

--- src/pages/Login/_components/kakao.tsx
+++ src/pages/Login/_components/kakao.tsx
@@ -1,17 +1,14 @@
-import KakaoIcon from '@/assets/kakaoIcon.svg';
-
 const Kakao = () => {
   const handleLogin = () => {
     // Spring Security OAuth2 엔드포인트로 리다이렉트, 추후 배포 시 다른 주소로 변경
     window.location.href = 'http://34.47.125.208:8080/api/oauth2/authorization/kakao';
-    // window.location.href = 'http:/localhost:8080/api/oauth2/authorization/kakao';
+    // window.location.href = 'http://localhost:8080/api/oauth2/authorization/kakao';
   };
 
   return (
     <button className="btn-kakao" onClick={handleLogin}>
       <div className="inner-container">
-        <img className="kakao-icon-img" src={KakaoIcon} alt="kakao" />
-        카카오톡으로 로그인
+        카카오로 시작하기
       </div>
     </button>
   );
