f41787318f64883f48c93078698cde212171e4e2
gimgyuwon <gimgyuwon2@gmail.com>
2025-09-06 20:53:42 +0900

    fix: api endpoint 변경

--- src/constant/api.ts
+++ src/constant/api.ts
@@ -1,6 +1,6 @@
 export const API_ENDPOINTS = {
-  SIGNUP: 'http://34.47.125.208:8080/api/auth/signup',
-  ALL_MEMBER: 'http://34.47.125.208:8080/api/v1/members',
+  SIGNUP: "https://34.47.125.208/api/v1/auth/signup",
+  ALL_MEMBER: "https://34.47.125.208/api/v1/members",
 } as const;
 
-export type ApiEndpoint = typeof API_ENDPOINTS[keyof typeof API_ENDPOINTS];
+export type ApiEndpoint = (typeof API_ENDPOINTS)[keyof typeof API_ENDPOINTS];
