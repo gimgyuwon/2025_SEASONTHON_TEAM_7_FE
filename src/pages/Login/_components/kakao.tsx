c32440fb0727609fed87c1affe7da5210e36fb9c
hardwoong <dew04300@naver.com>
2025-09-07 04:19:47 +0900

    fix: 로그인 리다이렉트 주소 수정

--- src/pages/Login/_components/kakao.tsx
+++ src/pages/Login/_components/kakao.tsx
@@ -1,15 +1,14 @@
 const Kakao = () => {
   const handleLogin = () => {
     // Spring Security OAuth2 엔드포인트로 리다이렉트, 추후 배포 시 다른 주소로 변경
-    window.location.href = 'http://34.47.125.208:8080/api/oauth2/authorization/kakao';
+    // window.location.href = 'http://34.47.125.208:8080/api/oauth2/authorization/kakao';
+    window.location.href = "https://tee-talk.com/api/v1/auth/signup";
     // window.location.href = 'http://localhost:8080/api/oauth2/authorization/kakao';
   };
 
   return (
     <button className="btn-kakao" onClick={handleLogin}>
-      <div className="inner-container">
-        카카오로 시작하기
-      </div>
+      <div className="inner-container">카카오로 시작하기</div>
     </button>
   );
 };
